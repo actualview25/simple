e852f277addb145607eb5118b8d84c320cfa9f3d
ActualView <actual.view25@gmail.com>
2025-12-24 14:46:19 +0300

    add virtual tour

---
+++ data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "الرئيسية",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.876226643019848,
+        "pitch": 0.2881230819013787,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.142090864523544,
+          "pitch": -0.4226677648965822,
+          "rotation": 15.707963267948973,
+          "target": "1--"
+        },
+        {
+          "yaw": -0.8696261244432701,
+          "pitch": -0.4227341082549749,
+          "rotation": 3.141592653589793,
+          "target": "2---"
+        },
+        {
+          "yaw": -0.6158667841208167,
+          "pitch": -0.40433550179559674,
+          "rotation": 3.141592653589793,
+          "target": "3---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.39064274485824413,
+          "pitch": -0.35784612712823005,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "كنب برتقالي",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.8975979010256516,
+        "pitch": 0.27859835192117544,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8850553037864088,
+          "pitch": -0.4109645360577652,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "كنب جلدي أسود",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.8665192121022187,
+        "pitch": 0.3023603754956614,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.871389572482915,
+          "pitch": -0.40459019916785266,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "تصميم كلاسيكي قاتم",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.878007470646093,
+        "pitch": 0.22383115453493474,
+        "fov": 1.489667856106396
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8832058962926084,
+          "pitch": -0.4043219546843453,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
